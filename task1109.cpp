5475a74b5b3bd38b3d010923600857d4b387e868
alishercpp <alisher.abdimuminov.2005@gmail.com>
2023-09-16 19:09:55 +0500

    init

---
+++ task1109.cpp
@@ -0,0 +1,11 @@
+// author: Ali
+// date: 16/09/2023
+#include <bits/stdc++.h>
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::cout << "The next number for the number " << n << " is " << n + 1 << "." << std::endl;
+    std::cout << "The previous number for the number " << n << " is " << n - 1 << ".";
+    return 0;
+}
