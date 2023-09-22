fd6db3d2d087ab27eadf2162957fc859fbf7141c
alishercpp <alisher.abdimuminov.2005@gmail.com>
2023-09-22 16:05:01 +0500

    Uch baqaloq

---
+++ task754.cpp
@@ -0,0 +1,10 @@
+// author: Ali
+// date  : 22/09/2023
+#include <bits/stdc++.h>
+
+int main () {
+    int a, b, c;
+    std::cin >> a >> b >> c;
+    a <= 727 and a >= 94 and b <= 727 and b >= 94 and c <= 727 and c >= 94 ? std::cout << std::max({a, b, c}) : std::cout << "Error";
+    return 0;
+}
