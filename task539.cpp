5e07b3a67b399ae4b2af6433a8c8529785dde7d0
alishercpp <alisher.abdimuminov.2005@gmail.com>
2023-09-22 15:52:19 +0500

    init

---
+++ task539.cpp
@@ -0,0 +1,10 @@
+// author: Ali
+// date  : 22/09/2023
+#include <bits/stdc++.h>
+
+int main() {
+    int n;
+    std::cin >> n;
+    n % 2 == 0 ? std::cout << n / 2 : std::cout << n;
+    return 0;
+}
