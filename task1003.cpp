a811cf7e8e078c2b62e77b33f7051a6da99b0cfe
alishercpp <alisher.abdimuminov.2005@gmail.com>
2023-09-27 17:35:52 +0500

    magazin

---
+++ task1003.cpp
@@ -0,0 +1,11 @@
+// author: Ali
+// date  : 27/09/2023
+#include <bits/stdc++.h>
+
+int main() {
+    int x, y, z;
+    std::cin >> x >> y >> z;
+    int q = 3, r = 5, f = 12;
+    std::cout << x * q + y * r + z * f;
+    return 0;
+}
